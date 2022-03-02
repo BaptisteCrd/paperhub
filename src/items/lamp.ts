cad20b6bed0686bb97701698924357f3c26d518f
Baptiste CERDAN <baptiste.cerdan@etu.unistra.fr>
2022-03-02 12:23:19 +0100

    :hammer: modified lamp.ts

--- src/items/lamp.ts
+++ src/items/lamp.ts
@@ -1,6 +1,7 @@
 import { Icon, icon } from '@fortawesome/fontawesome-svg-core';
 import { faLightbulb } from '@fortawesome/free-solid-svg-icons';
 import { Item } from '../item/item';
+import * as paper from 'paper';
 
 export class Lamp implements Item {
     public readonly isSizable : boolean;
@@ -14,10 +15,17 @@ export class Lamp implements Item {
     }
 
     drawItem() {
+      var circle = new paper.Path.Circle(new paper.Point(0, -3), 30);
+      circle.fillColor = new paper.Color("#33c1ff");
+      circle.opacity = .7;
 
+      var text = new paper.PointText(new paper.Point(0, 0));
+      text.justification = 'center';
+      text.fillColor = new paper.Color('black');;
+      text.content = this.title;
     }
 
     move() {
-      
+
     }
 }
